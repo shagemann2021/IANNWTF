23ec199dfdcbab033bc5fe00a9a45035bbf6557e
Samuel <samuel.hagemann@gmx.de>
2021-11-06 12:13:32 +0100

    update data set

--- week2/data.py
+++ week2/data.py
@@ -2,40 +2,35 @@ import numpy as np
 
 # data for and
 def load_data_and(self):
-    data = [[1,0,0],
-            [0,1,0],
-            [1,1,1],
-            [0,0,0]]
-    return data
+    return np.array([[1,0,0],
+                    [0,1,0],
+                    [1,1,1],
+                    [0,0,0]])
 
 # data for or
 def load_data_or(self):
-    data = [[1,0,1],
-            [0,1,1],
-            [1,1,1],
-            [0,0,0]]
-    return data
+    return np.array([[1,0,1],
+                    [0,1,1],
+                    [1,1,1],
+                    [0,0,0]])
 
 # data for not and
 def load_data_notand(self):
-    data = [[1,0,1],
-            [0,1,1],
-            [1,1,0],
-            [0,0,1]]
-    return data
+    return np.array([[1,0,1],
+                    [0,1,1],
+                    [1,1,0],
+                    [0,0,1]])
 
 # data for not or
 def load_data_notor(self):
-    data = [[1,0,0],
-            [0,1,0],
-            [1,1,0],
-            [0,0,1]]
-    return data
+    return np.array([[1,0,0],
+                    [0,1,0],
+                    [1,1,0],
+                    [0,0,1]])
 
 # data for xor
 def load_data_xor(self):
-    data = [[1,0,1],
-            [0,1,1],
-            [1,1,0],
-            [0,0,0]]
-    return data
+    return np.array([[1,0,1],
+                    [0,1,1],
+                    [1,1,0],
+                    [0,0,0]])
